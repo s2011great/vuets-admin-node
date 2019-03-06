a2176f4a4c5498f947f9504e78d157b023a0ef45
shanglibo <shanglibo@gbase.cn>
2019-03-06 17:36:13 +0800

    添加“获取用户信息”接口，封装sql查询函数。

--- index.js
+++ index.js
@@ -14,20 +14,37 @@ app.use(bodyParser.json())
 /**
  * 连接MySQL数据库
  */
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
   password : 'root',
   database : 'vuetssql'
 })
-connection.connect(function(err) {
-  if (err) {
-    console.error('数据库连接失败' + err.message)
-    return
-  }
- 
-  console.log('数据库连接成功！')
-})
+/**
+ * 数据库查询函数
+ * 
+ * @param {*} sql 
+ */
+let sqlQuery =  function(sql) {
+  // 返回Promise
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function(error, connection) {
+      if (error) {
+        reject(error)
+      } else {
+        connection.query(sql, function (error, rows) {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(rows)
+          }
+          // 结束会话
+          connection.release()
+        })
+      }
+    })
+  })
+}
 
 // 对所有进入的请求都提前做一层过滤，设置CORS
 app.all('*', (req, res, next) => {
@@ -77,40 +94,42 @@ app.post('/login', (req, res) => {
 })
 
 /**
- * 数据库查询函数
- * 
- * @param {*} sql 
- */
-function sqlQuery(sql) {
-  connection.query(sql, function (error, results, fields) {
-    if (error) throw error
-    return results
-  })
-}
-
-/**
  * 获取用户信息接口
  * 
  * @path: /info
  * @method: get
  * @params: { token }
  */
-app.get('/info', (req, res) => {
+app.get('/info', async (req, res) => {
   const token = req.query.token
   const { name: username } = jwt.verify(token, secretPrivateKey)
-  const userSqlData = sqlQuery(`SELECT * from user where user_name="${username}"`)
-  console.log(userSqlData)
-  const roleIdSqlData = sqlQuery(`SELECT role_id from user_role where user_id="${userSqlData[0].user_id}"`)
-  console.log(userSqlData, roleIdSqlData)
-  res.send({
-    code: 200,
-    data: {
-      name: 'admin',
-      userId: '1',
-      roles: ['admin', 'superadmin']
-    },
-    message: '请求成功！'
-  })
+  try {
+    const userSqlData = await sqlQuery(`SELECT * from user where user_name="${username}"`)
+    const roleIdSqlData = await sqlQuery(`SELECT role_id from user_role where user_id="${userSqlData[0].user_id}"`)
+    let roles = []
+    const name = userSqlData[0].name
+    const userId = userSqlData[0].user_id
+    for (let i = 0; i < roleIdSqlData.length; i++) {
+      const roleId = roleIdSqlData[i].role_id
+      let role = await sqlQuery(`SELECT name from role where id="${roleId}"`)
+      roles.push(role[0].name)
+    }
+    res.send({
+      code: 200,
+      data: {
+        name,
+        userId,
+        roles,
+      },
+      message: '请求成功！'
+    })
+  } catch (error) {
+    res.send({
+      code: 500,
+      data: {},
+      message: error,
+    })
+  }
 })
 
 // 监听端口
